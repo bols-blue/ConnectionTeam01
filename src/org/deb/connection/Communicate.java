310730a1d10305b9049765687f1ed8c72982c203
bols <bols@cyezarin.fugue.com>
2010-01-19 00:22:31 +0900

    add connection Class and bluetooth sample code

---
+++ src/org/deb/connection/Communicate.java
@@ -0,0 +1,18 @@
+package org.deb.connection;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import backport.android.bluetooth.BluetoothAdapter;
+import backport.android.bluetooth.BluetoothDevice;
+
+public class Communicate implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
