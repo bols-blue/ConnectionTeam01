c2ebe27d6c0ee89dab7a2f343717600e23c8f588
bols <bols@cyezarin.fugue.com>
2010-01-19 00:21:17 +0900

    add Communicate Class

--- src/org/deb/connection/TopActivity.java
+++ src/org/deb/connection/TopActivity.java
@@ -1,6 +1,10 @@
 package org.deb.connection;
 
+import backport.android.bluetooth.BluetoothAdapter;
+import backport.android.bluetooth.BluetoothDevice;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,17 +13,19 @@ import android.widget.Button;
 
 public class TopActivity extends Activity implements OnClickListener {
 	final String TAG = "DebCon";
-	
+
 	Button bluetoothButton;
 	Button handwritingButton;
-	
+	BluetoothAdapter mLocalDevice;
+	static final private int REQUEST_ENABLE_BT = 100;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
-		bluetoothButton = (Button)findViewById(R.id.bluetooth_button);
-		handwritingButton = (Button)findViewById(R.id.handwriting_button);
+
+		bluetoothButton = (Button) findViewById(R.id.bluetooth_button);
+		handwritingButton = (Button) findViewById(R.id.handwriting_button);
 		bluetoothButton.setOnClickListener(this);
 		handwritingButton.setOnClickListener(this);
 	}
@@ -29,13 +35,92 @@ public class TopActivity extends Activity implements OnClickListener {
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
 		case R.id.bluetooth_button:
-			//TODO start bluetooth activity
+			init();
+			// TODO start bluetooth activity
 			Log.v(TAG, "Start Bluetooth Activity");
 			break;
 		case R.id.handwriting_button:
-			//TODO start handwriting activity
+			// TODO start handwriting activity
 			Log.v(TAG, "Start Handwriting Activity");
 			break;
 		}
+	}
+
+
+	/**
+	 * bluetoothデバイスの有無を調べてある場合はmLocalDeviceにセットする。
+	 * 
+	 * @return デバイスがある場合 true
+	 */
+	boolean ensureSupported() {
+		BluetoothAdapter locDev = BluetoothAdapter.getDefaultAdapter();
+
+		if (locDev == null) {
+			return false;
+		}
+
+		mLocalDevice = locDev;
+		return true;
+	}
+
+	/**
+	 * BTデバイスが有効にする
+	 * 
+	 * @return 有効になっている場合true ユーザーに問い合わせをして有効ににする場合 false
+	 */
+	boolean ensureEnabled() {
+		boolean enabled = mLocalDevice.isEnabled();
+
+		if (enabled) {
+			return true;
+		}
+
+		Intent enableBt = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		startActivityForResult(enableBt, REQUEST_ENABLE_BT);
+		return false;
+	}
+
+	protected void init() {
+		int a;
+		a=10;
+		if (this.ensureSupported()) {
+			;
+		}
+		if (this.ensureSupported()) {
+			;
+		}
+
+	}
+
+	/**
+	 * 
+	 * @param resultCode
+	 * @param data
+	 */
+	void onRequestEnableResult(int resultCode, Intent data) {
+		boolean enabled = (resultCode == Activity.RESULT_OK);
+
+		// ACTION_REQUEST_ENABLE returns incorrect resultCode on Platform2.0.
+		int sdkInt = Build.VERSION.SDK_INT;
+
+		if (sdkInt > Build.VERSION_CODES.DONUT) {
+			enabled = (resultCode == Activity.RESULT_CANCELED);
+		}
+
+	}
+
+	/**
+	 * 
+	 * @param address
+	 * @return
+	 */
+	BluetoothDevice obtainRemoteDevice(String address) {
+		BluetoothDevice remDev = null;
+
+		try {
+			remDev = mLocalDevice.getRemoteDevice(address);
+		} catch (IllegalArgumentException invalidBtAddr) {
+		}
+		return remDev;
 	}
 }
